4649864636c0773007455473e0e074391f6565b1
mvkoroteev <mvkoroteev@fa.ru>
2019-04-04 12:40:56 +0300

    Simple menu added

--- app/src/main/java/com/example/menudemojava/MainActivity.java
+++ app/src/main/java/com/example/menudemojava/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.menudemojava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+
+        menu.add("menu1");
+        menu.add("menu2");
+        menu.add("menu3");
+        menu.add("menu4");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
 }
