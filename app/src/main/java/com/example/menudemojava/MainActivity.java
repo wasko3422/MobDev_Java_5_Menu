1c7f9647d994beb0fb78e8bb97a449c715448897
mvkoroteev <mvkoroteev@fa.ru>
2019-04-04 12:41:57 +0300

    Menu handler added

--- app/src/main/java/com/example/menudemojava/MainActivity.java
+++ app/src/main/java/com/example/menudemojava/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.menudemojava;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onCreateOptionsMenu(menu);
     }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
 
 }
