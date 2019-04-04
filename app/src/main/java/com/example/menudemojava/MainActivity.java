2b8822605c89f3587bc2f97643626dfebd18ec8d
mvkoroteev <mvkoroteev@fa.ru>
2019-04-04 12:44:02 +0300

    Menu with grouping added

--- app/src/main/java/com/example/menudemojava/MainActivity.java
+++ app/src/main/java/com/example/menudemojava/MainActivity.java
@@ -4,26 +4,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Элементы экрана
+    TextView tv;
+    CheckBox chb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // находим элементы
+        tv = (TextView) findViewById(R.id.textView);
+        chb = (CheckBox) findViewById(R.id.chbExtMenu);
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // TODO Auto-generated method stub
-
-        menu.add("menu1");
-        menu.add("menu2");
-        menu.add("menu3");
-        menu.add("menu4");
+        // добавляем пункты меню
+        menu.add(0, 1, 0, "add");
+        menu.add(0, 2, 0, "edit");
+        menu.add(0, 3, 3, "delete");
+        menu.add(1, 4, 1, "copy");
+        menu.add(1, 5, 2, "paste");
+        menu.add(1, 6, 4, "exit");
 
         return super.onCreateOptionsMenu(menu);
     }
+
 
     public boolean onOptionsItemSelected(MenuItem item) {
         // TODO Auto-generated method stub
